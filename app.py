45a7c4d15a30bc83613ca53b10e52a47b66dd152
mattp15 <matthewrphillips15@gmail.com>
2022-11-04 10:48:16 -0400

    maybe app.py is actually finished now

--- app.py
+++ app.py
@@ -28,6 +28,13 @@ def load_user(userid):
     except models.DoesNotExist:
         pass
 
+app.register_blueprint(users, url_prefix='/user')
+app.register_blueprint(recipes, url_prefix='/user/recipes')
+app.register_blueprint(user_things, url_prefix='/user/index')
+
+app.route('/')
+def testing():
+    return "test successful"
 
 if __name__ == '__main__':
     models.initialize()
--- resources/user.py
+++ resources/user.py
@@ -5,3 +5,11 @@ from flask_login import login_user, current_user, logout_user
 from playhouse.shortcuts import model_to_dict
 
 users = Blueprint('users', 'users')
+
+@users.route('/', methods=["GET"])
+def show_users():
+    return jsonify(
+        data = 'blah blah',
+        message = "Test successful",
+        status = 200
+    ), 200
