135c0cbe6dc0bf2c8f8dd02c521f3f8df10aa7cb
mattp15 <matthewrphillips15@gmail.com>
2022-11-19 15:34:15 -0500

    Recipes successfully populate from array

--- resources/recipes.py
+++ resources/recipes.py
@@ -1,4 +1,5 @@
-import models, random
+import models, random 
+import recipes_objects
 from flask import Blueprint, request, jsonify
 from playhouse.shortcuts import model_to_dict
 
@@ -45,20 +46,30 @@ def get_recipe(id):
 
 @recipes.route('/add', methods=["POST"])
 def create_recipe():
-    payload = request.get_json()
-    try:
-        models.Recipes.get(models.Recipes.title == payload['title'])
-        return jsonify(
-            data={},
-            message = "That recipe already exists in the database",
-            status = 409
-        ), 409
-    except models.DoesNotExist:
-        recipe = models.Recipes.create(**payload)
-        recipe_dict = model_to_dict(recipe)
-        return jsonify(
-            data = recipe_dict,
-            message = "Recipe succesfully added to database",
-            status = 201
-        ), 201
+    for i in range(len(recipes_objects.recipes)):
+        try:
+            models.Recipes.get(models.Recipes.title == recipes_objects.recipes[i]['title'])
+        except models.DoesNotExist:          
 
+            recipe = models.Recipes.create(
+                author_credit = recipes_objects.recipes[i]['author_credit'],
+                calories = recipes_objects.recipes[i]['calories'],
+                carbs = recipes_objects.recipes[i]['carbs'],
+                category = recipes_objects.recipes[i]['category'],
+                description = recipes_objects.recipes[i]['description'],
+                fat = recipes_objects.recipes[i]['fat'],
+                image = recipes_objects.recipes[i]['image'],
+                ingredients = recipes_objects.recipes[i]['ingredients'],
+                instructions = recipes_objects.recipes[i]['instructions'],
+                protein = recipes_objects.recipes[i]['protein'],
+                servings = recipes_objects.recipes[i]['servings'],
+                shopping_list = recipes_objects.recipes[i]['shopping_list'],
+                time = recipes_objects.recipes[i]['time'],
+                title = recipes_objects.recipes[i]['title'],
+            )
+            recipe_dict = model_to_dict(recipe)
+    return jsonify(
+        data = recipe_dict,
+        message = "Recipe succesfully added to database",
+        status = 201
+    ), 201
